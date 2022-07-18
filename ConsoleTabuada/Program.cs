a413417699d9a46741c665c0d84fc4602ab1d383
Gustavo Taglianetti <taglisouza.0304@hotmail.com>
2022-07-18 09:46:51 -0300

    @GustavoTagli[exercise_tabuada]

---
+++ ConsoleTabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleTabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+
+            Console.WriteLine("Insira um número:");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                result = num * i;
+                Console.WriteLine(num + " x " + i + " = " + result);
+            }
+        }
+    }
+}
